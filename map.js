6661bead93b2066a467a87149ee63e1d86fc7e63
lseseri <lhseseri@gmail.com>
2025-02-18 14:24:40 -0800

    add first map

---
+++ map.js
@@ -0,0 +1,12 @@
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoibHNlc2VyaSIsImEiOiJjbTdiMWJlMWQwMTVuMmtvbnZwZGYyYmliIn0.A7V6OB5lL3CoX4_VCp3Bjw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+});
